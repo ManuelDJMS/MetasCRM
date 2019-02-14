90f0aef5f646b7f1ff781c6e7e70d4238fa03231
Luis Azael Jimenez Fajardo <sistemas2@metas.com.mx>
2019-02-14 15:08:30 -0600

    ,

--- FrmHOME.vb
+++ FrmHOME.vb
@@ -233,8 +233,4 @@
         Panel6.BackColor = Color.DimGray
         Panel8.BackColor = Color.DimGray
     End Sub
-
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
-
-    End Sub
 End Class
