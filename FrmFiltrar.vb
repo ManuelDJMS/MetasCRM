bb5ba92c19bfa3d2e508371fba476ee318001b7a
ManuelDJMS <bechy10@live.com.mx>
2019-02-21 09:06:55 -0600

    Update FrmFoliosDelAnioAnterior.Designer.vb

--- FrmFiltrar.vb
+++ FrmFiltrar.vb
@@ -45,77 +45,71 @@
         End Try
     End Sub
     Private Sub dgEmpresas_RowHeaderMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles dgEmpresas.RowHeaderMouseClick
-
         Try
-            MetodoMetasInf2018()
-            comando2018 = conexion2018.CreateCommand
+            MetodoMetasInf2019()
+            comando2019 = conexion2019.CreateCommand
             Dim R As String
-            R = "select distinct Folio, ClavecontactoConsign, RazonSocial, Compania, RFC, DomicilioConsig, PaisConsig, CiudadConsig, EdoConsig, CPConsig
+            R = "select distinct ClavecontactoConsign, RazonSocial, Compania, RFC, DomicilioConsig, PaisConsig, CiudadConsig, EdoConsig, CPConsig
                         from [INFORMES-SERVICIOS] INNER JOIN MetAsInf on [INFORMES-SERVICIOS].ClavecontactoConsign=MetAsInf.Clavempresa
                         where ClavecontactoConsign =" & dgEmpresas.Rows(e.RowIndex).Cells(0).Value.ToString()
-            comando2018.CommandText = R
-            lector2018 = comando2018.ExecuteReader
-            lector2018.Read()
-            If ((lector2018(0) Is DBNull.Value) OrElse (lector2018(0) Is Nothing)) Then
-                FrmFoliosDelAnioAnterior.TextFolio.Text = "-"
-            Else
-                FrmFoliosDelAnioAnterior.TextFolio.Text = lector2018(0)
-            End If
-            If ((lector2018(1) Is DBNull.Value) OrElse (lector2018(1) Is Nothing)) Then
+            comando2019.CommandText = R
+            lector2019 = comando2019.ExecuteReader
+            lector2019.Read()
+            If ((lector2019(0) Is DBNull.Value) OrElse (lector2019(0) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbClave.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbClave.Text = lector2018(1)
+                FrmFoliosDelAnioAnterior.lbClave.Text = lector2019(0)
             End If
-            If ((lector2018(2) Is DBNull.Value) OrElse (lector2018(2) Is Nothing)) Then
+            If ((lector2019(1) Is DBNull.Value) OrElse (lector2019(1) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbRazonSocial.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbRazonSocial.Text = lector2018(2)
+                FrmFoliosDelAnioAnterior.lbRazonSocial.Text = lector2019(1)
             End If
-            If ((lector2018(3) Is DBNull.Value) OrElse (lector2018(3) Is Nothing)) Then
+            If ((lector2019(2) Is DBNull.Value) OrElse (lector2019(2) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbEmpresa.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbEmpresa.Text = lector2018(3)
+                FrmFoliosDelAnioAnterior.lbEmpresa.Text = lector2019(2)
             End If
-            If ((lector2018(4) Is DBNull.Value) OrElse (lector2018(4) Is Nothing)) Then
+            If ((lector2019(3) Is DBNull.Value) OrElse (lector2019(3) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbRFC.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbRFC.Text = lector2018(4)
+                FrmFoliosDelAnioAnterior.lbRFC.Text = lector2019(3)
             End If
-            If ((lector2018(5) Is DBNull.Value) OrElse (lector2018(5) Is Nothing)) Then
+            If ((lector2019(4) Is DBNull.Value) OrElse (lector2019(4) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbDomicilio.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbDomicilio.Text = lector2018(5)
+                FrmFoliosDelAnioAnterior.lbDomicilio.Text = lector2019(4)
             End If
-            If ((lector2018(6) Is DBNull.Value) OrElse (lector2018(6) Is Nothing)) Then
+            If ((lector2019(5) Is DBNull.Value) OrElse (lector2019(5) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbPais.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbPais.Text = lector2018(6)
+                FrmFoliosDelAnioAnterior.lbPais.Text = lector2019(5)
             End If
-            If ((lector2018(7) Is DBNull.Value) OrElse (lector2018(7) Is Nothing)) Then
+            If ((lector2019(6) Is DBNull.Value) OrElse (lector2019(6) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbCiudad.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbCiudad.Text = lector2018(7)
+                FrmFoliosDelAnioAnterior.lbCiudad.Text = lector2019(6)
             End If
-            If ((lector2018(8) Is DBNull.Value) OrElse (lector2018(8) Is Nothing)) Then
+            If ((lector2019(7) Is DBNull.Value) OrElse (lector2019(7) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbEstado.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbEstado.Text = lector2018(8)
+                FrmFoliosDelAnioAnterior.lbEstado.Text = lector2019(7)
             End If
-            If ((lector2018(9) Is DBNull.Value) OrElse (lector2018(9) Is Nothing)) Then
+            If ((lector2019(8) Is DBNull.Value) OrElse (lector2019(8) Is Nothing)) Then
                 FrmFoliosDelAnioAnterior.lbCP.Text = "-"
             Else
-                FrmFoliosDelAnioAnterior.lbCP.Text = lector2018(9)
+                FrmFoliosDelAnioAnterior.lbCP.Text = lector2019(8)
             End If
-            lector2018.Close()
+            lector2019.Close()
             R = "SELECT distinct isnull([INFORMES-SERVICIOS].ClavecontactoConsign, '-'),isnull([INFORMES-SERVICIOS].TIPO,'-') as Tipo, isnull([INFORMES-SERVICIOS].MARCA,'-')as Marca, isnull([INFORMES-SERVICIOS].MODELO,'-') as Modelo,
               isnull([INFORMES-SERVICIOS].Serie,'-')as Serie,isnull([INFORMES-SERVICIOS].ID,'-')as ID,isnull([INFORMES-SERVICIOS].ServCatalogo1,'-')as ServCatalogo1,isnull([INFORMES-SERVICIOS].ServCatalogo2,'-')as ServCatalogo2,
               isnull([INFORMES-SERVICIOS].PUCalib,0)as PUCalib, isnull([INFORMES-SERVICIOS].PULab,0) as PULab from [INFORMES-SERVICIOS] where [INFORMES-SERVICIOS].ClavecontactoConsign =" & dgEmpresas.Rows(e.RowIndex).Cells(0).Value.ToString()
-            comando2018.CommandText = R
-            lector2018 = comando2018.ExecuteReader
-            While lector2018.Read()
-                FrmFoliosDelAnioAnterior.DGServicios.Rows.Add(False, lector2018(0), lector2018(1), lector2018(2), lector2018(3), lector2018(4), lector2018(5), lector2018(6), lector2018(7), lector2018(8), lector2018(9))
+            comando2019.CommandText = R
+            lector2019 = comando2019.ExecuteReader
+            While lector2019.Read()
+                FrmFoliosDelAnioAnterior.DGServicios.Rows.Add(False, lector2019(0), lector2019(1), lector2019(2), lector2019(3), lector2019(4), lector2019(5), lector2019(6), lector2019(7), lector2019(8), lector2019(9))
             End While
-            lector2018.Close()
+            lector2019.Close()
             FrmFoliosDelAnioAnterior.lbServicios.Text = "Total de Servicios: " + Convert.ToString(FrmFoliosDelAnioAnterior.DGConsulta.Rows.Count - 1)
             Me.Dispose()
         Catch ex As Exception
