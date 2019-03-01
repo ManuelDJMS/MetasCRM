6757184f63a3a2c593a0a07e6c29a1b53cbfb37a
ManuelDJMS <bechy10@live.com.mx>
2019-03-01 09:37:20 -0600

    Creacion de Btacora

---
+++ Bitcora.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+Module Bitcora
+    ' Public conexionBit As String = "Data source=ASUSMANUEL; initial catalog=Bitacora; integrated security=true"
+    Public Sub Bitacora(Formulario As String, Evento As String, error1 As String, Descripcion As String)
+        Try
+            Dim conexionbit As New SqlConnection("Data Source=SERVER3\COMPAC2;Initial Catalog=METASINF-2019-3; User Id=sa; Password=Met99011578a;Integrated Security=False")
+            conexionbit.Open()
+            Dim comando As SqlCommand = conexionbit.CreateCommand()
+            Dim r As String
+            Dim I As Integer
+            Dim x As String
+            'Dim fechas As String
+            'fechas = DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss")
+            x = "'"
+            For I = 1 To Len(error1)
+                If Mid(error1, I, 1) = Chr(39) Then
+                    x = x & "'"
+                Else
+                    x = x & Mid(error1, I, 1)
+                End If
+            Next
+            error1 = x
+            r = "insert into Resultados(Formulario, Evento, Error, Descripcion, Fecha) values ('" & Formulario & "','" & Evento & "'," & error1 & "','" & Descripcion & "', getdate())"
+            comando.CommandText = r
+            comando.ExecuteNonQuery()
+            conexionbit.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error del Sistema")
+        End Try
+    End Sub
+End Module
