359266f552ba9c551ebe6033d368dac2bc6b605c
ManuelDJMS <bechy10@live.com.mx>
2019-03-01 09:41:31 -0600

    Eliminacion de form innutilizable

--- Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'ConexionLocal()
-        'autoCompletar(TextBox1.Text, TextBox1)
-
-    End Sub
-
-
-End Class
