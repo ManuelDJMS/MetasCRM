f3a81244380ce1afcf37462852e4475c12f393cd
Manuel Morales <46793411+ManuelDJMS@users.noreply.github.com>
2019-05-09 15:06:31 -0500

    Update frmEdicionCot2018-2019.vb

--- frmEdicionCot2018-2019.vb
+++ frmEdicionCot2018-2019.vb
@@ -198,11 +198,11 @@ Public Class frmEdicionCot2018_2019
         MetodoMetasCotizador()
         Dim maximo As Integer
         Dim R As String
-        Dim comando As New SqlCommand("select MAX(Numcot) from [INFORMES-SERVICIOS]", conexionMetasCotizador)
+        Dim comando As New SqlCommand("select MAX(Numcot) from [Cotizaciones]", conexionMetasCotizador)
         Dim lector As SqlDataReader
         lector = comando.ExecuteReader
         lector.Read()
-        maximo = lector(0)
+        maximo = lector(0) + 1
         lector.Close()
         comandoMetasCotizador = conexionMetasCotizador.CreateCommand
         fechaActual = Convert.ToDateTime(DTPDesde.Text).ToShortDateString
@@ -221,91 +221,20 @@ Public Class frmEdicionCot2018_2019
         comandoMetasCotizador.ExecuteNonQuery()
 
         For i = 0 To DGCotizaciones.Rows.Count - 2
-            R = "insert into DetalleCotizaciones (NumCot,EquipId, PartidaNo,Cantidad, CantidadReal) values (" & maximo & "',
-                '" & "-" & "'," & Val(DGCotizaciones.Item(3, i).Value) & ",'" & DGCotizaciones.Item(4, i).Value & "',
-                '" & DGCotizaciones.Item(5, i).Value & "','" & DGCotizaciones.Item(6, i).Value & "','" & DGCotizaciones.Item(8, i).Value & "',
-                '" & DGCotizaciones.Item(7, i).Value & "'," & Val(DGCotizaciones.Item(10, i).Value) & "," & "0" & ")"
+            R = "insert into DetalleCotizaciones (NumCot,EquipId, PartidaNo,Cantidad, CantidadReal) values (" & maximo & ",'" & DGCotizaciones.Item(1, i).Value & "'," & Val(DGCotizaciones.Item(0, i).Value) & ",
+                " & Val(DGCotizaciones.Item(3, i).Value) & "," & Val(DGCotizaciones.Item(9, i).Value) & ")"
             comandoMetasCotizador.CommandText = R
             comandoMetasCotizador.ExecuteNonQuery()
         Next i
         ''--------------------------------------------------------------------------------------------------------------------------------
 
-        MsgBox("Guardado en 2019 correctamente.", MsgBoxStyle.Information)
+        MsgBox("Guardado correctamente.", MsgBoxStyle.Information)
 
         FrmCotizacion2018.txtClave.Text = ""
         FrmCotizacion2018.txtNombreE.Text = ""
         FrmCotizacion2018.DGCotizaciones.DataSource = Nothing
         FrmCotizacion2018.DGEmpresas.DataSource = Nothing
         Me.Dispose()
-        '''--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        'MetodoMetasCotizador()
-        'comandoLIMS = conexionLIMS.CreateCommand
-        'fechaActual = Convert.ToDateTime(DTPDesde.Text).ToShortDateString
-        'fecharecepcion = Convert.ToDateTime(DTPHasta.Text).ToShortDateString
-        'R = "insert into Cotizaciones (idContacto, Origen, idLugarCondicion, idCuandoCondicion, idModalidadCondicion, 
-        '    idTiempoEntregaCondicion, idPagoCondicion, idLeyendaCondicion, idValidezCondicion, idMonedaCondicion, idDocumentoCondicion, 
-        '    idModoCont, Referencia, FechaDesde, FechaHasta, Observaciones, idUsuarioCotizacion, idLugarDeCalibracion, Subtotal, IVA, Total)
-        '     values (" & Val(txtCveContacto.Text) & ",'" & origen & "'," & Val(cboServicio.Tag) & "," & Val(Cbcuando.Tag) & "," & Val(CbModalidad.Tag) & "," & Val(CboTiempo.Tag) & "," &
-        '     Val(CCondPago.Tag) & "," & Val(CboLeyenda.Tag) & "," & Val(CboValidez.Tag) & "," & Val(CboMoneda.Tag) & "," & Val(ComboDocCond.Tag) & "," & Val(CboContabilizar.Tag) & ",'" &
-        '     txtReferencia.Text & "','" & fechaActual & "','" & fecharecepcion & "','" & txtObservaciones.Text & "'," & Val(txtCotizo2019.Text) & "," & cbl & ""
-        'Try
-        '    '----------------------------A partir de aqui se busca en LIMS la empresa....si no se agrega-------------------------------------
-        '    Dim R As String
-        '    Dim empresa As Integer
-        '    '//////////////////Aqui se busca la empresa en lims\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-        '    MetodoLIMS()
-        '    comandoLIMS = conexionLIMS.CreateCommand
-        '    R = "select isnull(CustomerId,'-') from SetupCustomerDetails where KeyFiscal='" & txtNumCond.Text & "' or Organization='" & txtNombreEmpresa.Text & "'"
-        '    lectorLIMS = comandoLIMS.ExecuteReader
-        '    lectorLIMS.Read()
-        '    empresa = lectorLIMS(0)
-        '    lectorLIMS.Close()
-        '    conexionLIMS.Close()
-        '    '/////////////////////////////////////////////////////////////////////////////////
-        '    If empresa = "-" Then '------------Decision para guardar o no la empresa------------------------
-        '        MetodoLIMS()
-        '        R = "select "
-
-        '        fechaActual = Convert.ToDateTime(DTPDesde.Text).ToShortDateString
-        '        R = "insert into EntradaRegistroCot (NumCot, Cliente, ClaveContacto, Fecha, Referencia, Numcond, Observaciones, ServicioEn, TipodeCliente, 
-        '            CveEmpresa, [Elaboró Cot], ModoDeContabilizar) values (" & txtNombreEmpresa.Text & "',
-        '            " & Val(txtCveContacto.Text) & ", (CONVERT(varchar(10), getdate(), 103)),'" & txtReferencia.Text & "'," & Val(txtNumCond.Text) & ",
-        '            '" & txtObservaciones.Text & "','" & cboServicio.Text & "'," & empresa & "," & Val(txtTelefono.Text) & ",
-        '            " & Val(txtCotizo2019.Text) & "," & txtConta.Text & ")"
-        '        comandoMetasCotizador.CommandText = R
-        '        comandoMetasCotizador.ExecuteNonQuery()
-        '        'Codigo para guardar en 1Cotizar----------------------------------------------------
-        '        For i = 0 To DGCotizaciones.Rows.Count - 2
-        '            R = "insert into [1Cotizar] (Numcot, PartidaNo, ServCatalogo, Especial, Cant, Tipo, Marca, Modelo, Alcance, 
-        '             ID, Punitariocot, Realizado) values (" & DGCotizaciones.Item(2, i).Value & "',
-        '            '" & "-" & "'," & Val(DGCotizaciones.Item(3, i).Value) & ",'" & DGCotizaciones.Item(4, i).Value & "',
-        '            '" & DGCotizaciones.Item(5, i).Value & "','" & DGCotizaciones.Item(6, i).Value & "','" & DGCotizaciones.Item(8, i).Value & "',
-        '            '" & DGCotizaciones.Item(7, i).Value & "'," & Val(DGCotizaciones.Item(10, i).Value) & "," & "0" & ")"
-        '            comandoMetasCotizador.CommandText = R
-        '            comandoMetasCotizador.ExecuteNonQuery()
-        '        Next i
-        '    End If
-        '    '--------------------------------------------------------------------------------------------------------------------------------
-
-        '    MsgBox("Guardado en 2019 correctamente.", MsgBoxStyle.Information)
-
-        '    FrmCotizacion2018.txtClave.Text = ""
-        '    FrmCotizacion2018.txtNombreE.Text = ""
-        '    FrmCotizacion2018.DGCotizaciones.DataSource = Nothing
-        '    FrmCotizacion2018.DGEmpresas.DataSource = Nothing
-        '    Me.Dispose()
-        'Catch ex As Exception
-        '    MsgBox(ex.Message, MsgBoxStyle.Critical, "Error del Sistema")
-        '    cadena = Err.Description
-        '    cadena = cadena.Replace("'", "")
-        '    Bitacora("frmEdicionCot2018-2019", "Error al momento de guardar", Err.Number, cadena)
-        'End Try
-
-        ''--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        MsgBox(CbModalidad.Tag.ToString)
-
-        '''--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        'MsgBox(CbModalidad.Tag.ToString)
 
     End Sub
 
