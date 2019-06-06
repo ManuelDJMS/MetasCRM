28b798527d42c45d2f547f0a70dca1af37fd19bd
Manuel Morales <46793411+ManuelDJMS@users.noreply.github.com>
2019-06-06 16:43:10 -0500

    Update frmEdicionCot2018-2019.vb

--- frmEdicionCot2018-2019.vb
+++ frmEdicionCot2018-2019.vb
@@ -234,12 +234,14 @@ Public Class frmEdicionCot2018_2019
              values (" & maximo & "," & Val(txtCveContacto.Text) & ",'" & origen & "'," & Val(cboServicio.Tag) & "," & Val(Cbcuando.Tag) & "," & Val(CbModalidad.Tag) & "," & Val(CboTiempo.Tag) & "," &
              Val(CCondPago.Tag) & "," & Val(CboLeyenda.Tag) & "," & Val(CboValidez.Tag) & "," & Val(CboMoneda.Tag) & "," & Val(ComboDocCond.Tag) & "," & Val(CboContabilizar.Tag) & ",'" &
              txtReferencia.Text & "','" & fechaActual & "','" & fecharecepcion & "','" & txtObservaciones.Text & "'," & Val(txtCotizo2019.Text) & "," & subtotal & "," & iva & "," & total & ")"
+        MsgBox(R)
         comandoMetasCotizador.CommandText = R
         comandoMetasCotizador.ExecuteNonQuery()
 
         For i = 0 To DGCotizaciones.Rows.Count - 2
             R = "insert into DetalleCotizaciones (NumCot,EquipId, PartidaNo,Cantidad, CantidadReal) values (" & maximo & "," & DGCotizaciones.Item(10, i).Value & "," & Val(DGCotizaciones.Item(0, i).Value) & ",
                 " & Val(DGCotizaciones.Item(3, i).Value) & "," & Val(DGCotizaciones.Item(9, i).Value) & ")"
+            MsgBox(R)
             comandoMetasCotizador.CommandText = R
             comandoMetasCotizador.ExecuteNonQuery()
         Next i
